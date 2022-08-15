0cb011c2cf0c73a240f01b73ff39fbfe35972191
Miya Sasamoto <miya.19991225@gmail.com>
2022-08-15 13:02:29 +0900

    finish task1_1

---
+++ task1_1.java
@@ -0,0 +1,23 @@
+public class task1_1{
+
+  public static void main(String[] args){
+
+    //int型のhogeIntを宣言、50を初期化
+    int hogeInt = 50;
+    // StringクラスのhogeStringの変数宣言、テストを初期化
+    String hogeString = "テスト";
+    //小数点を扱っているからdouble。
+    double hogeDouble = 0.2;
+    //変数hogeIntを100に上書き
+    hogeInt = 100;
+    //hogeSteingを合格に上書き
+    hogeString = "合格";
+    //hogeintの値を表示
+    System.out.println(hogeInt);
+    //hogeStringの値を開業して表示
+    System.out.println(hogeString);
+    //hogeDoubleの値表示
+    System.out.println(hogeDouble);
+
+  }
+}
